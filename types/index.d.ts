93243389c89b97a74bdcea0296a160ba88211301
enzocomics <enzo@cheerupemokid.com>
2024-11-20 11:20:10 -0800

    added module declaration

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,8 @@
-export let Client: typeof import("./Client").Client;
-export let RequestHelper: typeof import("./RequestHelper").RequestHelper;
-export let FIELD_NOT_VERIFIED: string;
-export let FIELD_VALID: string;
-export let FIELD_INVALID: string;
+declare module "@mosparo/api-client" {
+	export let Client: typeof import("./Client").Client;
+	export let RequestHelper: typeof import("./RequestHelper").RequestHelper;
+	export let FIELD_NOT_VERIFIED: string;
+	export let FIELD_VALID: string;
+	export let FIELD_INVALID: string;
+}
 //# sourceMappingURL=index.d.ts.map
