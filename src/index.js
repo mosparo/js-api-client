9a3130489aa1a5fc5264ff417c3f3d8916128250
Matthias Zobrist <matthias.zobrist@zepi.net>
2025-01-01 19:56:53 +0100

    Export the StatisticResult and VerificationResult classes. With the exported classes, it's possible to check for them like with `instanceof`.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 module.exports = {
     Client: require('./Client').Client,
     RequestHelper: require('./RequestHelper').RequestHelper,
+    StatisticResult: require('./StatisticResult').StatisticResult,
+    VerificationResult: require('./VerificationResult').VerificationResult,
     FIELD_NOT_VERIFIED: require('./VerificationResult').FIELD_NOT_VERIFIED,
     FIELD_VALID: require('./VerificationResult').FIELD_VALID,
     FIELD_INVALID: require('./VerificationResult').FIELD_INVALID,
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,8 @@
-declare module "@mosparo/api-client" {
-	export let Client: typeof import("./Client").Client;
-	export let RequestHelper: typeof import("./RequestHelper").RequestHelper;
-	export let FIELD_NOT_VERIFIED: string;
-	export let FIELD_VALID: string;
-	export let FIELD_INVALID: string;
-}
+export let Client: typeof import("./Client").Client;
+export let RequestHelper: typeof import("./RequestHelper").RequestHelper;
+export let StatisticResult: typeof import("./StatisticResult").StatisticResult;
+export let VerificationResult: typeof import("./VerificationResult").VerificationResult;
+export let FIELD_NOT_VERIFIED: string;
+export let FIELD_VALID: string;
+export let FIELD_INVALID: string;
 //# sourceMappingURL=index.d.ts.map
